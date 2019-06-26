8a365b9c48ccdb3132d5d466a5059527c8723cb2
Elias <eliasebrahimpour@gmail.com>
2019-06-26 17:51:21 +0430

    better clients scripts

--- clients/commandlines/tradingexpense.sh
+++ clients/commandlines/tradingexpense.sh
@@ -2,4 +2,18 @@
 
 source config.sh
 
-curl --data "token=$TOKEN&amount=$1&text=$2" $BASE_URL/submit/expense/
+print_usage()
+{
+  echo "Use this script to submit expense reports tp ${BASE_URL}"
+  echo "Usage: ${0} <Amount> <Description>. Eg:"
+  echo "Usage: ${0} 1000 Donation"
+}
+
+AMOUNT=$1
+shift
+TEXT=$*
+if [ -z "$TEXT" ] ; then
+  print_usage
+fi
+
+curl --data "token=$TOKEN&amount=$AMOUNT&text=$TEXT" $BASE_URL/submit/expense/
