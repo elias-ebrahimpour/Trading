44545a9a65da049c7d6f518ad2c1c4e13ac9f667
Elias <eliasebrahimpour@gmail.com>
2019-06-26 17:25:05 +0430

    / is Selective in urls

--- WebCost/urls.py
+++ WebCost/urls.py
@@ -2,11 +2,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^submit/expense/$', views.submit_expense, name='submit_expense'),
-    url(r'^submit/income/$', views.submit_income, name='submit_income'),
-    url(r'^accounts/register/$', views.register, name='register'),
-    url(r'^q/generalstat/$', views.generalstat, name='generalstat'),
-    url(r'^accounts/login/$', views.login, name='login'),
-    url(r'^accounts/logout/$', views.logout, name="logout"),
-    url(r'^', views.homepage, name='homepage'),
+    url(r'^submit/expense/?$', views.submit_expense, name='submit_expense'),
+    url(r'^submit/income/?$', views.submit_income, name='submit_income'),
+    # url(r'^accounts/register/$', views.register, name='register'),
+    url(r'^q/generalstat/?$', views.generalstat, name='generalstat'),
+    url(r'^accounts/login/?$', views.login, name='login'),
+    url(r'^accounts/logout/?$', views.logout, name="logout"),
+    url(r'^', views.register, name='register'),
 ]
